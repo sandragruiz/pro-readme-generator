86b9f0f421bef7b42069ea95edf7bb28acad2d62
unknown <sandra.gutierrez.ru@gmail.com>
2023-03-27 02:26:36 -0500

    js files starter code

---
+++ index.js
@@ -0,0 +1,16 @@
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// Create an array of questions for user input
+const questions = [];
+
+// Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
