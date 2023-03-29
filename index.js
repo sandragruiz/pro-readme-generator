f4fadae31e008478ee9b1bb0fee6d598b48a16e4
unknown <sandra.gutierrez.ru@gmail.com>
2023-03-29 15:21:04 -0500

    added input prompts, writeFile function, generateMarkdown.js

--- index.js
+++ index.js
@@ -1,16 +1,74 @@
-// Include packages needed for this application
+// Packages for application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
 // Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+// Function to create README file with array of questions
+function createReadme() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description for your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What are the usage instructions?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Enter the license for your project:',
+    },
+    {
+     type:'input',
+     name: 'contributing',
+     message: 'Enter contribution guidelines for your project:',
+    },
+    {
+     type: 'input',
+     name: 'tests',
+     message: 'Enter instructions for testing:'
+    },
+    {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ]);
+}
+
+// Function to write README file with user input
+fs.writeFile('README.md', content, (err) => {
+    if (err) throw err;
+    console.log('README file has been created!');
+  }).catch((error) => {
+    console.error(error);
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,80 @@
+// Generate markdown //
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+
+  const encodedLicense = encodeURI(license);
+  return `![License](https://img.shields.io/badge/license-${encodedLicense}-green.svg)`;
+}
 
 // Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  const encodedLicense = encodeURI(license);
+  return `[${license}](https://opensource.org/licenses/${encodedLicense})`;
+}
 
 // Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `## License
+
+This project is licensed under the ${renderLicenseLink(license)} license. See the [LICENSE](./LICENSE) file for details.
+
+`;
+}
 
 // Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contribution Guidelines
+
+${data.contributing}
+
+## Testing
+
+${data.tests}
+
+## Questions 
+
+For questions or concerns, please contact [${answers.githubUsername}](https://github.com/${answers.githubUsername}) at ${answers.email}.
 
 `;
 }
